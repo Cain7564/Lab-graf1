b346f10300fa4c6de027b7426d46ddaba35e50ea
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2025-07-12 12:49:00 -0600

    poligono cuatro

--- src/main.rs
+++ src/main.rs
@@ -30,26 +30,34 @@ fn scanline_fill(fb: &mut Framebuffer, poly: &[Vector2], color: Color) {
     }
 }
 fn main() {
-    let mut fb = Framebuffer::new(600, 600, Color::WHITE);
+    let mut fb = Framebuffer::new(2000, 2000, Color::WHITE);
     fb.set_background_color(Color::WHITE);
-    fb.set_current_color(Color::BLUE);
+    fb.set_current_color(Color::RED);
 
     let poly = vec![
-        Vector2::new(165.0, 380.0),
-        Vector2::new(185.0, 360.0),
-        Vector2::new(180.0, 330.0),
-        Vector2::new(207.0, 345.0),
-        Vector2::new(233.0, 330.0),
-        Vector2::new(230.0, 360.0),
-        Vector2::new(250.0, 380.0),
-        Vector2::new(220.0, 385.0),
-        Vector2::new(205.0, 410.0),
-        Vector2::new(193.0, 383.0),
+        Vector2::new(413.0, 177.0),
+        Vector2::new(448.0, 159.0),
+        Vector2::new(502.0, 88.0),
+        Vector2::new(553.0, 53.0),
+        Vector2::new(535.0, 36.0),
+        Vector2::new(676.0, 37.0),
+        Vector2::new(660.0, 52.0),
+        Vector2::new(750.0, 145.0),
+        Vector2::new(761.0, 179.0),
+        Vector2::new(672.0, 192.0),
+        Vector2::new(659.0, 214.0),
+        Vector2::new(615.0, 214.0),
+        Vector2::new(632.0, 230.0),
+        Vector2::new(580.0, 230.0),
+        Vector2::new(597.0, 215.0),
+        Vector2::new(552.0, 214.0),
+        Vector2::new(517.0, 144.0),
+        Vector2::new(466.0, 180.0),
     ];
 
 
     // Rellenar el polígono con scanline
-    scanline_fill(&mut fb, &poly, Color::BLUE);
+    scanline_fill(&mut fb, &poly, Color::YELLOW);
 
     // Dibujar los lados del polígono
     for window in poly.windows(2) {
@@ -59,6 +67,6 @@ fn main() {
     }
     line(&mut fb, poly[poly.len() - 1], poly[0]); // cerrar el polígono
 
-    fb.render_to_file("poligono.png");
-    println!("Polígono dibujado y guardado como 'poligono.png'!");
+    fb.render_to_file("poligono4.png");
+    println!("Polígono dibujado y guardado como 'poligono4.png'!");
 }
