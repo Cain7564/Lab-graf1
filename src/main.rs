95f4f6f691eb5a1aa4af535bb7e954520ae22f03
Cain7564 <156337459+Cain7564@users.noreply.github.com>
2025-07-12 12:37:25 -0600

    Segundo poligono

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,18 @@ fn scanline_fill(fb: &mut Framebuffer, poly: &[Vector2], color: Color) {
 fn main() {
     let mut fb = Framebuffer::new(600, 600, Color::WHITE);
     fb.set_background_color(Color::WHITE);
-    fb.set_current_color(Color::BLUE);
+    fb.set_current_color(Color::RED);
 
     let poly = vec![
-        Vector2::new(165.0, 380.0),
-        Vector2::new(185.0, 360.0),
-        Vector2::new(180.0, 330.0),
-        Vector2::new(207.0, 345.0),
-        Vector2::new(233.0, 330.0),
-        Vector2::new(230.0, 360.0),
-        Vector2::new(250.0, 380.0),
-        Vector2::new(220.0, 385.0),
-        Vector2::new(205.0, 410.0),
-        Vector2::new(193.0, 383.0),
+        Vector2::new(321.0, 335.0),
+        Vector2::new(288.0, 286.0),
+        Vector2::new(339.0, 251.0),
+        Vector2::new(374.0, 302.0),
     ];
 
 
     // Rellenar el polígono con scanline
-    scanline_fill(&mut fb, &poly, Color::BLUE);
+    scanline_fill(&mut fb, &poly, Color::RED);
 
     // Dibujar los lados del polígono
     for window in poly.windows(2) {
